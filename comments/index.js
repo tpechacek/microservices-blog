b1855844cb27a358d455de0181f0a91ed23bcf4e
Taylor Pechacek <taylortpechacek@gmail.com>
2020-12-31 15:15:00 +1100

    finished up example app with microservices. Also, added a simple event bus that handles missing events. Moving onto next application and more complex scenarios now

--- comments/index.js
+++ comments/index.js
@@ -23,7 +23,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const comments = commentsByPostId[req.params.id] || [];
 
   // Push in the new comment we are trying to great into the array.
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   // Assign comments array back to the given post by post id.
   commentsByPostId[req.params.id] = comments;
@@ -35,6 +35,7 @@ app.post('/posts/:id/comments', async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending',
     },
   });
 
@@ -42,8 +43,32 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
